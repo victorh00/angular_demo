79394e265e890ce1ba08ccd865bfef4dc27856ac
Victor Hugo <victorvh06@gmail.com>
2025-08-11 02:21:51 -0300

    navigation/router-outlet working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/core/header/header.component';
 import { FooterComponent } from './components/core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { BancoComponent } from './components/features/banco/banco.component';
 import { ChecklistComponent } from './components/features/checklist/checklist.component';
 import { CalculadoraComponent } from './components/features/calculadora/calculadora.component';
 import { CronometroComponent } from './components/features/cronometro/cronometro.component';
+import { HomeComponent } from './components/core/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },  
   { path: 'calculadora', component: CalculadoraComponent },
   { path: 'cronometro', component: CronometroComponent },
   { path: 'checklist', component: ChecklistComponent },
